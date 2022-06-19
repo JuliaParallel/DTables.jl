137eaedabf83c1ef659222da938456686a54ec83
krynju <gulinski.krystian@gmail.com>
2022-06-19 13:55:33 +0200

    add env info to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using DTables
 using Test
 
+using Distributed
+
+@info(
+    "Execution environment details",
+    julia_version=VERSION,
+    n_workers=Distributed.nworkers(),
+    n_procs=Distributed.nprocs(),
+    n_threads=Threads.nthreads(),
+)
+
 @testset "DTables.jl" begin
     include("table.jl")
     include("table_dataframes.jl")
