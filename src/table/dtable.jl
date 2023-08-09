b2eab20b1bc2ef041539be224428e57203dbb18e
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-09 17:14:46 +0530

    Adding spaces.

--- src/table/dtable.jl
+++ src/table/dtable.jl
@@ -4,6 +4,7 @@ const VTYPE = Vector{Union{Dagger.Chunk,Dagger.EagerThunk}}
     DTable
 
 Structure representing the distributed table based on Dagger.
+
 The table is stored as a vector of `Chunk` structures which hold partitions of the table.
 That vector can also store `Dagger.EagerThunk` structures when an operation that modifies
 the underlying partitions was applied to it (currently only `filter`).
@@ -16,6 +17,7 @@ end
 
 DTable(chunks::Vector, tabletype) = DTable(VTYPE(chunks), tabletype, nothing)
 DTable(chunks::Vector, tabletype, schema) = DTable(VTYPE(chunks), tabletype, schema)
+
 """
     DTable(table; tabletype=nothing) -> DTable
 
